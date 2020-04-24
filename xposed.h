13b75f51a821e30c14d8e40644b99e0306aba13a
jackjsonlv <jackjsonlv@gmail.com>
2020-04-24 22:21:22 +0800

    zposed

--- xposed.h
+++ xposed.h
@@ -11,15 +11,15 @@
   #define XPOSED_LIB_DIR "/system/lib/"
 #endif
 #define XPOSED_LIB_DALVIK        XPOSED_LIB_DIR "libxposed_dalvik.so"
-#define XPOSED_LIB_ART           XPOSED_LIB_DIR "libxposed_art.so"
-#define XPOSED_JAR               "/system/framework/XposedBridge.jar"
+#define XPOSED_LIB_ART           XPOSED_LIB_DIR "libzposed_art.so"
+#define XPOSED_JAR               "/system/framework/ZposedBridge.jar"
 #define XPOSED_JAR_NEWVERSION    XPOSED_DIR "bin/XposedBridge.jar.newversion"
 #define XPOSED_LOAD_BLOCKER      XPOSED_DIR "conf/disabled"
 #define XPOSED_SAFEMODE_NODELAY  XPOSED_DIR "conf/safemode_nodelay"
 #define XPOSED_SAFEMODE_DISABLE  XPOSED_DIR "conf/safemode_disable"
 
-#define XPOSED_CLASS_DOTS_ZYGOTE "de.robv.android.xposed.XposedBridge"
-#define XPOSED_CLASS_DOTS_TOOLS  "de.robv.android.xposed.XposedBridge$ToolEntryPoint"
+#define XPOSED_CLASS_DOTS_ZYGOTE "com.rdc.zposed.ZposedBridge"
+#define XPOSED_CLASS_DOTS_TOOLS  "com.rdc.zposed.ZposedBridge$ToolEntryPoint"
 
 #if XPOSED_WITH_SELINUX
 #include <selinux/selinux.h>
